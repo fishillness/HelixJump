7465432490db3ebbd52807988894b195a24bd4a4
Mariya Kolodizhanskaya <fishillness@gmail.com>
2022-11-23 17:37:58 +0700

    Script "Segment"
    Added script "Segment"

---
+++ Assets/HelixJumpFS/Scripts/Segment.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public enum SegmentType
+{
+    Default,
+    Trap,
+    Empty,
+    Finish
+}
+
+
+public class Segment : MonoBehaviour
+{
+    [SerializeField] private SegmentType type;
+
+    public SegmentType Type => type;
+}
