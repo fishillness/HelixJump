4dd7489b898d5f9b6fd1631144d210763daf5955
Mariya Kolodizhanskaya <fishillness@gmail.com>
2022-11-28 21:32:01 +0700

    Script "ScoresCollector"
    Added script "ScoresCollector".

---
+++ Assets/HelixJumpFS/Scripts/Managers/ScoresCollector.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ScoresCollector : BallEvents
+{
+    [SerializeField] private LevelProgress levelProgress;
+    [SerializeField] private int scores;
+    public int Scores => scores;
+
+    protected override void OnBallCollisionSegment(SegmentType type)
+    {
+        if (type == SegmentType.Empty)
+        {
+            scores += levelProgress.CurrentLevel;
+        }
+
+    }
+}
