256658659bf84cf710c52fc6d8e4cf615da3fb36
Mariya Kolodizhanskaya <fishillness@gmail.com>
2022-11-29 19:01:27 +0700

    Script "PaletteSelection"
    Added script "PaletteSelection"

---
+++ Assets/HelixJumpFS/Scripts/PaletteSelection.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PaletteSelection : BallEvents
+{
+    [SerializeField] private Material axisMat;
+    [SerializeField] private Material ballMat;
+    [SerializeField] private Material defaultSegmentMat;
+    [SerializeField] private Material finishSegmentMat;
+    [SerializeField] private Material trapSegmentMat;
+
+    private int random;
+    private static int amountPalette = 3;
+
+    [SerializeField] private Color[] axisColors = new Color[amountPalette];
+    [SerializeField] private Color[] ballColors = new Color[amountPalette];
+    [SerializeField] private Color[] defaultSegmentColors = new Color[amountPalette];
+    [SerializeField] private Color[] finishSegmentColors = new Color[amountPalette];
+    [SerializeField] private Color[] trapSegmentColors = new Color[amountPalette];
+
+
+    private void Start()
+    {
+        ChangePalette();
+    }
+
+    public void ChangePalette()
+    {
+
+        random = Random.Range(0, amountPalette);
+        axisMat.color = axisColors[random];
+        ballMat.color = ballColors[random];
+        defaultSegmentMat.color = defaultSegmentColors[random];
+        finishSegmentMat.color = finishSegmentColors[random];
+        trapSegmentMat.color = trapSegmentColors[random];
+    }
+}
