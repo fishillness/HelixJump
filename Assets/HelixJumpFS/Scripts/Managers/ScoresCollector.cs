2b273f9a2b922528ed5e8c62b98762c4761c8360
Mariya Kolodizhanskaya <fishillness@gmail.com>
2022-11-29 20:37:07 +0700

    script "ScoreCollector"
    Added combo points

--- Assets/HelixJumpFS/Scripts/Managers/ScoresCollector.cs
+++ Assets/HelixJumpFS/Scripts/Managers/ScoresCollector.cs
@@ -5,12 +5,17 @@ public class ScoresCollector : BallEvents
     [SerializeField] private LevelProgress levelProgress;
     [SerializeField] private int scores;
     public int Scores => scores;
-
+    private int emptySegmentCombo = 1;
     protected override void OnBallCollisionSegment(SegmentType type)
     {
         if (type == SegmentType.Empty)
         {
-            scores += levelProgress.CurrentLevel;
+            scores += levelProgress.CurrentLevel * emptySegmentCombo;
+            emptySegmentCombo++;
+        }
+        if (type != SegmentType.Empty)
+        {
+            emptySegmentCombo = 1;
         }
     }
 
